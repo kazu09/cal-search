da708c6cb47475318f1a59fa08b1a3d789369eeb
kazu09 <kita.k50902@gmail.com>
2020-05-24 23:58:08 +0900

    5/24 commit

---
+++ app/controllers/concerns/tweets_controller.rb
@@ -0,0 +1,26 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to root_path
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :text, :image,:data)
+  end
+
+end
