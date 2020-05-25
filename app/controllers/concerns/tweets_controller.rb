9728591ededa5ba9509b5e7e5519bc1cee05a5c4
kazu09 <kita.k50902@gmail.com>
2020-05-26 01:34:58 +0900

    5/25 commit

--- app/controllers/concerns/tweets_controller.rb
+++ app/controllers/concerns/tweets_controller.rb
@@ -17,10 +17,25 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to tweet_path(tweet.id)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
 
   private
   def tweet_params
-    params.require(:tweet).permit(:title, :text, :image,:data)
+    params.require(:tweet).permit(:title, :text, :image, :data)
   end
 
 end
