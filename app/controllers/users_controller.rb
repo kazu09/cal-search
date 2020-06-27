0d672e984566c78c2c3bc2de69e260f9bb62df64
kazu09 <kita.k50902@gmail.com>
2020-06-27 19:30:19 +0900

    viewの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
   # マイページの表示ログイン後の表示等
+before_action :authenticate_user!, except: [:show]
+before_action :move_to_index, except: [:index]
+
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.order('day DESC')
   end
+
+  private
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+  
 end
 
