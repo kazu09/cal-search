83f03c525fae125e4f02842b52625eb79d7aec0d
kazu09 <kita.k50902@gmail.com>
2020-06-29 10:20:11 +0900

    validation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  validates :nickname , presence: true
+  # メールアドレスのバリデーション
+  validates :email, {presence: true}
+  validates :email, format: { with:/\A\S+@\S+\.\S+\z/}
+  validates :password, format: { with:/\A[0-9]+\z/}
 end
